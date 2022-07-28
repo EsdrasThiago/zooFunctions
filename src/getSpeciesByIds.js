2d73645ff496ee52e8efcc15071c53b5048dfee5
Esdras <esdras12thiago@gmail.com>
2022-07-28 18:32:36 -0300

    Requisito 1 Concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
-
-function getSpeciesByIds(ids) {
+const { species } = data
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (typeof ids === 'undefined') {
+    return [];
+  }
+  return ids.map((id) => species.find((especies) => especies.id === id))
 }
-
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
