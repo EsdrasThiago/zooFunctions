050a55a4d6b03b73fd0ed2e066d78a41ce41a9d1
Esdras <esdras12thiago@gmail.com>
2022-07-28 19:24:26 -0300

    Requisito 2 Concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
-
+const { species } = data
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const chosenAnimal = species.find((residentes) => residentes.name === animal)
+  return chosenAnimal.residents.every((residente) => residente.age > age)
 }
-
+console.log(getAnimalsOlderThan('bears', 2))
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -7,5 +7,4 @@ function getSpeciesByIds(...ids) {
   }
   return ids.map((id) => species.find((especies) => especies.id === id))
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
