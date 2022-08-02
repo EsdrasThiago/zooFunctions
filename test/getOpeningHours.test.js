afd4f39abfd6de24670e60b52027633e3070aad0
Esdras <esdras12thiago@gmail.com>
2022-08-02 17:13:50 -0300

    Requisito 12 concluido

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,13 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Testa se esta fechado na segunda as 9 horas', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
+  });
+  it('Testa se esta aberto na terça as 9 horas', () => {
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+  });
+  it('Testa se esta aberto na quarta as 9 horas', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
+  });
+});
