90d2d76ffeb122116dc3759d12359c68f3329d7d
Esdras <esdras12thiago@gmail.com>
2022-08-05 18:03:10 -0300

    Funções do requisito 9 criadas

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const dias = Object.keys(data.hours)
+const animais = data.species
+const getAnimalDays = (scheduleTarget) => {
+  if (animais.some((especies) => especies.name === scheduleTarget)) {
+  const animal = animais.find((animais) => {
+    return animais.name === scheduleTarget;
+  });
+  return animal.availability;
+}};
+
+const allDays = (scheduleTarget) => {
+  return 'oi'
 }
 
+const animalDay = (scheduleTarget) => {
+  if (dias.some((dia) => dia === scheduleTarget)) {
+  }
+  
+}
+// console.log(Object.keys(data.hours));
+
+function getSchedule(scheduleTarget) {
+  // seu código aqui
+  if (getAnimalDays(scheduleTarget)) return getAnimalDays(scheduleTarget);
+  if (animalDay(scheduleTarget)) return animalDay(scheduleTarget);
+  return allDays(scheduleTarget)
+};
+console.log(getSchedule());
 module.exports = getSchedule;
