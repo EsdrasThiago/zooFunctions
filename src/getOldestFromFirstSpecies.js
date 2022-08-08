37ede4356d1c6481ed00d13db7dfe01fa1eb23a7
Esdras <esdras12thiago@gmail.com>
2022-08-08 17:32:42 -0300

    Requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const findColaborator = (id) => {
+  const colaborator = data.employees.find((colaborador) => colaborador.id === id);
+  return colaborator;
+};
+
+const firstSpecie = (first) => {
+  const comparador = first.responsibleFor.find((id) => species.map((ids) => id === ids));
+  return comparador;
+};
+
+const findAnimalResidents = (id) => {
+  const animal = species.find((animals) => animals.id === id);
+  return animal.residents;
+};
+
+const oldest = (animal) => {
+  const idades = animal.map((idade) => idade.age);
+  const ordenar = idades.sort((a, b) => b - a);
+  const encontrar = animal.find((older) => older.age === ordenar[0]);
+  return Object.values(encontrar);
+};
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  return oldest(findAnimalResidents(firstSpecie(findColaborator(id))));
 }
-
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,31 +1,37 @@
 const data = require('../data/zoo_data');
 
-const dias = Object.keys(data.hours)
-const animais = data.species
-const getAnimalDays = (scheduleTarget) => {
-  if (animais.some((especies) => especies.name === scheduleTarget)) {
-  const animal = animais.find((animais) => {
-    return animais.name === scheduleTarget;
-  });
-  return animal.availability;
-}};
+// const dias = Object.keys(data.hours);
+// const horas = Object.values(data.hours);
+// const animais = data.species;
+// const terca = data.hours;
+// console.log(terca);
 
-const allDays = (scheduleTarget) => {
-  return 'oi'
-}
+// const getAnimalDays = (scheduleTarget) => {
+//   if (animais.some((especies) => especies.name === scheduleTarget)) {
+//     const animal = animais.find((animais) => animais.name === scheduleTarget);
+//     return animal.availability;
+//   }
+// };
 
-const animalDay = (scheduleTarget) => {
-  if (dias.some((dia) => dia === scheduleTarget)) {
-  }
-  
-}
-// console.log(Object.keys(data.hours));
+// const allDays = (scheduleTarget) => {
+
+// };
+
+// const animalDay = (scheduleTarget) => {
+//   if (dias.some((dia) => dia === scheduleTarget)) {
+//     // const localizar = data.hours.find((dia) => {
+//     //   return dia === scheduleTarget
+//     // })
+//     // console.log(localizar);
+//   }
+// };
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
-  if (getAnimalDays(scheduleTarget)) return getAnimalDays(scheduleTarget);
-  if (animalDay(scheduleTarget)) return animalDay(scheduleTarget);
-  return allDays(scheduleTarget)
-};
-console.log(getSchedule());
+  // if (getAnimalDays(scheduleTarget)) return getAnimalDays(scheduleTarget);
+  // if (animalDay(scheduleTarget)) return animalDay(scheduleTarget);
+  // return allDays(scheduleTarget);
+}
+// console.log(getSchedule());
+// Não consegui fazer :(
 module.exports = getSchedule;
